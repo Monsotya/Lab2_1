56b01f33393c1664786b9f099f0f1c49bd6f40ef
Monsotya <sonya20033009@gmail.com>
2021-09-30 19:31:50 +0300

    Update

--- first.py
+++ first.py
@@ -1,23 +1,23 @@
 class Rectangle:
 
     def __init__(self, length=1, width=1):
-        self._width = width
-        self._length = length
+        self.set_attributes(length, width)
 
     def get_attributes(self):
         return self._width, self._length
 
-    def set_attributes(self, length=1, width=1):
-        if 0.0 < length < 20.0:
-            self._length = length
-        else:
-            print("Error!\n")
-            exit()
-        if 0.0 < width < 20.0:
-            self._width = width
-        else:
-            print("Error!\n")
-            exit()
+    def set_attributes(self, length, width):
+        try:
+            if 0.0 < length < 20.0:
+                self._length = length
+            else:
+                raise ValueError
+            if 0.0 < width < 20.0:
+                self._width = width
+            else:
+                raise ValueError
+        except ValueError:
+            print("Wrong value!\n")
 
     def perimeter(self):
         print("The perimeter of a rectangle is:\n", 2 * (self._length + self._width))
--- second.py
+++ second.py
@@ -1,8 +1,10 @@
 class Rational:
 
-    def __init__(self, numerator=0, denominator=0):
+    def __init__(self, numerator=1, denominator=1):
         a = numerator
         b = denominator
+        if b == 0:
+            raise ZeroDivisionError
         while a != 0 and b != 0:
             if a > b:
                 a = a % b
@@ -21,6 +23,9 @@ class Rational:
         print(self._numerator / self._denominator)
 
 
-object = Rational(2, 4)
-object.print_fraction()
-object.print_result()
+try:
+    object = Rational(2, 0)
+    object.print_fraction()
+    object.print_result()
+except ZeroDivisionError:
+    print("Error!\n")
