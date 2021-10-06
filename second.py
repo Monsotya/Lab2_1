59eb3927b1ea9bfae419a8dddfec2f90e541a623
Monsotya <sonya20033009@gmail.com>
2021-10-06 14:09:59 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,31 +1,29 @@
+from math import gcd
+
 class Rational:
 
     def __init__(self, numerator=1, denominator=1):
-        a = numerator
-        b = denominator
-        if b == 0:
-            raise ZeroDivisionError
-        while a != 0 and b != 0:
-            if a > b:
-                a = a % b
-            else:
-                b = b % a
-        if a + b != 0:
-            numerator /= (a + b)
-            denominator /= (a + b)
-        self._numerator = numerator
-        self._denominator = denominator
+        if not isinstance(numerator, int):
+            raise TypeError
+        if not isinstance(denominator, int):
+            raise TypeError
+        if not denominator:
+            raise ValueError
+        self._numerator = numerator // gcd(numerator, denominator)
+        self._denominator = denominator // gcd(numerator, denominator)
 
-    def print_fraction(self):
-        print(self._numerator, " / ", self._denominator)
+    def fraction(self):
+        return f'{self._numerator} / {self._denominator}'
 
-    def print_result(self):
-        print(self._numerator / self._denominator)
+    def result(self):
+        return self._numerator / self._denominator
 
 
 try:
-    object = Rational(2, 0)
-    object.print_fraction()
-    object.print_result()
-except ZeroDivisionError:
-    print("Error!\n")
+    object = Rational(3, 6)
+    print(object.fraction())
+    print(object.result())
+except TypeError:
+    print("Wrong type!\n")
+except ValueError:
+    print("Zero division!\n")
